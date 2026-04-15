6eab6072ff5329bb19d7c0a0a78365e953cc7bc9
celalkcc <malte.koc@mailbox.org>
2026-04-15 15:31:29 +0200

    Added display Module

--- include/display.hpp
+++ include/display.hpp
@@ -1,5 +1,12 @@
 #ifndef DISPLAY_HPP
 #define DISPLAY_HPP
 
+class Display {
+    public:
+        void init();
+        void clear();
+        void drawBall(int x, int y);
+        void update();
+};
 
 #endif
--- src/display.cpp
+++ src/display.cpp
@@ -1,3 +1,8 @@
 #include "display.hpp"
 
 // including display Libs
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
